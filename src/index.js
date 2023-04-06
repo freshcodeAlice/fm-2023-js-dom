2f7c777ad79322e23e12d260f8ae3bc92b3eda98
freshcodeAlice <freshjs.ofte@gmail.com>
2023-04-06 11:59:03 +0300

    classList

--- src/index.js
+++ src/index.js
@@ -1,34 +1,10 @@
-/*
-Створити структуру:
 
-header
-    nav
-        ul
-            li.list-item*5
-
-main
-    article
-        h1{Heading}
-        p>lorem50
-
-Не використовуючи css-файла, зробити заголовок фіолетовим і з розміром тексту 20пкс
-List-item вибудувати в рядочок, зробити їм фоновий колір рожевий
-
-Параграфу дати колір тексту синій
-
-
-
-*/
 
 const h1 = document.querySelector('h1');
-h1.style.color = 'violet';
-h1.style.fontSize = '20px';
+
 
 const liArr = document.querySelectorAll('li');
 liArr.forEach((li)=>{
-    li.style.display = 'inline-block';
-    li.style.backgroundColor = 'pink'
+    li.classList.add('new-list-item');
+    li.classList.remove('list-item');
 })
-
-const p = document.querySelector('p');
-p.style.color = 'blue';
