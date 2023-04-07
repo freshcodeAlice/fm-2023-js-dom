e1aeaf24dc7a8f79eb1b36de57d9e699f7e59ba9
freshcodeAlice <freshjs.ofte@gmail.com>
2023-04-07 09:53:01 +0300

    EventListener

--- src/index.js
+++ src/index.js
@@ -1,10 +1,33 @@
+// Подія
+
+/* Все, що стається на сторінці, все, чим користувач може взаємодіяти з нами - це події (event)
+Клік мишею, наведення мишею, натиснення клавіші, завантаження ресурсу
+*/
+
+const btn = document.querySelector('#btn');
+
+function clickHandler() {
+    alert('Button clicked');
+}
+
+btn.addEventListener('click', clickHandler);
 
 
-const h1 = document.querySelector('h1');
 
 
-const liArr = document.querySelectorAll('li');
-liArr.forEach((li)=>{
-    li.classList.add('new-list-item');
-    li.classList.remove('list-item');
-})
+/*
+Зробити кнопку
+і написати функцію, яка виводить кількість разів, скільки на цю кнопку вже натискали раніше
+*/
+
+let counter = 0;
+
+function getCountClicker() {
+    alert(++counter);
+
+    if (counter > 2) {
+        btn.removeEventListener('click', clickHandler);
+    }
+}
+
+btn.addEventListener('click', getCountClicker);
