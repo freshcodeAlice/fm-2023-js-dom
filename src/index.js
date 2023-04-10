5eda9411b4fdf2928b5aeacb7b0c4e8fd092c242
freshcodeAlice <freshjs.ofte@gmail.com>
2023-04-10 10:18:55 +0300

    key events moving

--- src/index.js
+++ src/index.js
@@ -1,36 +1,29 @@
 // Подія
 
-/*
-За натиснення на батьківський елемент (секцію gamefield) маємо перемістити дів box за тими координами, куди клацнув користувач
 
-*/
 
-let counter = 0;
+document.addEventListener('keydown', (event) => {
+    const SIZE = 10;
+    const box = event.currentTarget.body.children[0];
+   const x = box.offsetLeft;
+   const y = box.offsetTop;
+   switch(event.code) {
+        case "ArrowLeft": {
+            box.style.left = `${x - SIZE}px`;
+            break;
+        }
+        case "ArrowRight": {
+            box.style.left = `${x + SIZE}px`;
+            break;
+        }
+        case "ArrowUp": {
+            box.style.top = `${y - SIZE}px`;
+            break;
+        }
+        case "ArrowDown": {
+            box.style.top = `${y + SIZE}px`;
+            break;
+        }
 
-const score = document.querySelector('.score');
-const gamefield = document.querySelector('.gamefield');
-//const box = document.querySelector('.box');
-
-gamefield.addEventListener('click', function (event) {
-
-    if(event.target !== event.currentTarget) {
-        counter++;
-        updateView();
-    }  
-    
-    const box = event.currentTarget.children[0];
-
-    const x = getRandomCoordinate(0, event.currentTarget.offsetWidth) - (box.offsetWidth / 2);
-    const y = getRandomCoordinate(0, event.currentTarget.offsetHeight) - (box.offsetHeight / 2);
-    box.style.top = `${y}px`;
-    box.style.left = `${x}px`;
+    }
 })
-
-function getRandomCoordinate(min, max) {
-    return Math.random() * (max - min) + min;
-  }
-
-
-function updateView() {
-    score.textContent = counter;
-}
