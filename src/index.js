7d30c52fc8fb64b8afceeed5342e029b86b2d359
freshcodeAlice <freshjs.ofte@gmail.com>
2023-04-10 10:51:20 +0300

    show/hide password

--- src/index.js
+++ src/index.js
@@ -1,29 +1,13 @@
-// Подія
+const btn = document.querySelector('#show');
+const passInput = document.querySelector('#pass');
+
+const attr = document.createAttribute('type');
+passInput.setAttributeNode(attr);
 
 
+btn.addEventListener('click', function () {
+    attr.value = attr.value === 'password' ? 'text' : 'password';
 
-document.addEventListener('keydown', (event) => {
-    const SIZE = 10;
-    const box = event.currentTarget.body.children[0];
-   const x = box.offsetLeft;
-   const y = box.offsetTop;
-   switch(event.code) {
-        case "ArrowLeft": {
-            box.style.left = `${x - SIZE}px`;
-            break;
-        }
-        case "ArrowRight": {
-            box.style.left = `${x + SIZE}px`;
-            break;
-        }
-        case "ArrowUp": {
-            box.style.top = `${y - SIZE}px`;
-            break;
-        }
-        case "ArrowDown": {
-            box.style.top = `${y + SIZE}px`;
-            break;
-        }
+//    passInput.setAttribute('type', 'password');
 
-    }
 })
