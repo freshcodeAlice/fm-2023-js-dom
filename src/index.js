0c59d8a3a25a4855d74c5938638261be912fbbf7
freshcodeAlice <freshjs.ofte@gmail.com>
2023-04-10 09:41:36 +0300

    box game 1

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 // Подія
 
 /*
-Створити блок div і за натиснення на нього всередині вивести коордінати кліка
+За натиснення на батьківський елемент (секцію gamefield) маємо перемістити дів box за тими координами, куди клацнув користувач
 
 */
 
-const div = document.querySelector('.wrapper');
 
-div.addEventListener('click', function(event) {
-    event.target.textContent = `x:${event.clientX} y: ${event.clientY}`;
+const gamefield = document.querySelector('.gamefield');
+//const box = document.querySelector('.box');
+
+gamefield.addEventListener('click', function (event) {
+
+    if(event.target === event.currentTarget) {
+        const box = event.target.children[0];
+        console.dir(box);
+        const x = event.clientX - (box.offsetWidth / 2);
+        const y = event.clientY - (box.offsetHeight / 2);
+        box.style.top = `${y}px`;
+        box.style.left = `${x}px`;
+    }    
 })
