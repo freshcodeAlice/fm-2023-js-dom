1d6dead2adac4f9c43b0a14a3b10886ee36191cf
freshcodeAlice <freshjs.ofte@gmail.com>
2023-04-11 11:10:12 +0300

    one-way-red-circle

--- src/index.js
+++ src/index.js
@@ -5,10 +5,16 @@ btn.addEventListener('click', createSquare);
 
 
 function createSquare() {
-    const div = document.createElement('div'); // Створює елемент в пам'яті і повертає посилання на нього
-    document.body.append(div); // Приєднуємо до батьківського елемента, таким самим чином відображаючи елемент
-    div.classList.add('red-circle');
-    // div.append('text-text')
+    const div = document.querySelector('.red-circle');
+    if (div) {
+        div.remove();
+    } else {
+        const div = document.createElement('div'); // Створює елемент в пам'яті і повертає посилання на нього
+        document.body.append(div); // Приєднуємо до батьківського елемента, таким самим чином відображаючи елемент
+        div.classList.add('red-circle');
+        // div.append('text-text')
+    }
+   
 }
 
 
