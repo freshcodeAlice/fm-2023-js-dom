a73fe67732987e9a9397bfef7434469cb5886b19
freshcodeAlice <freshjs.ofte@gmail.com>
2023-04-11 11:15:54 +0300

    cool random circles

--- src/index.js
+++ src/index.js
@@ -5,20 +5,15 @@ btn.addEventListener('click', createSquare);
 
 
 function createSquare() {
-    const div = document.querySelector('.red-circle');
-    if (div) {
-        div.remove();
-    } else {
         const div = document.createElement('div'); // Створює елемент в пам'яті і повертає посилання на нього
         document.body.append(div); // Приєднуємо до батьківського елемента, таким самим чином відображаючи елемент
         div.classList.add('red-circle');
-        // div.append('text-text')
-    }
-   
+        const parentWidth = document.body.offsetWidth;
+        const parentHeight = document.body.offsetHeight;
+        div.style.top = `${getRandomCoordinates(0, parentHeight)}px`;
+        div.style.left = `${getRandomCoordinates(0,parentWidth)}px`;
 }
 
-
-/*
-За натиснення на кнопку створювати в body червоні кружечки
-
-*/
+function getRandomCoordinates(min, max) {
+        return Math.random() * (max - min) + min;
+}
