38f5962f8f92deae20512cdf3d372d7b55e8919e
freshcodeAlice <freshjs.ofte@gmail.com>
2023-04-11 09:56:43 +0300

    form submit handler

--- src/index.js
+++ src/index.js
@@ -3,10 +3,23 @@ const errorContainer = document.querySelector('.error-message');
 
 form.addEventListener('submit', function(event) {
     event.preventDefault();
+    errorContainer.textContent = '';
+    errorContainer.classList.remove('show-error');
 //    console.log(event);
     const {email: {value: emailValue}, pass: {value: passValue}} = event.target;
-   if(!emailValue.includes('@')) {
-    errorContainer.textContent = 'Email is not valid';
+//    if(!emailValue.includes('@')) {
+//     errorContainer.textContent = 'Email is not valid';
+//     errorContainer.classList.add('show-error');
+//    }
+   if(passValue.toLowerCase().includes('qwerty') || passValue.length <= 5) {
+    errorContainer.textContent = 'Pass is not valid';
     errorContainer.classList.add('show-error');
    }
 })
+
+
+/*
+Прийняти від користувача пароль і перевірити, чи не місить він слова "qwerty" і чи більший він за 5 символів
+ 
+
+*/
