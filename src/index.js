d3d6a8f595288a8d3c57aa7c00855195d0440506
freshcodeAlice <freshjs.ofte@gmail.com>
2023-04-12 12:00:04 +0300

    setInterval

--- src/index.js
+++ src/index.js
@@ -1,21 +1,10 @@
-const id = setTimeout(function () {
-    console.log('tick')
-}, 10000);
+const id = setInterval(function () {
+
+}, 1000);
 
 
 function tick() {
     console.log('tack');
 }
 
-
-
 tick();
-
-let j = 0;
-for (let i = 0; i < 100000000; i++) {
-    j++;    
-}
-
-console.log(j)
-
-clearTimeout(id);
