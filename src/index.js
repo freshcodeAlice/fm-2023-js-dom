7bf7bcc9e8a2149bcdd95e3c520d3aeb43436330
freshcodeAlice <freshjs.ofte@gmail.com>
2023-04-12 11:37:07 +0300

    setTimeout

--- src/index.js
+++ src/index.js
@@ -1,50 +1,21 @@
-/*
-
-Створити в розмітці form з інпутом і кнопкою
-
-За наснення на кнопку введене в інпут має додатись до певного масиву з даними, і відобразитися на екрані як елемент списку
+const id = setTimeout(function () {
+    console.log('tick')
+}, 10000);
 
 
-Задачка з *: додати кожному елементу списку кнопку, яка його видаляє.
-*/
-
-const list = document.querySelector('#list');
-const form = document.querySelector('#form');
-
-let todoArray = [];
-
-form.addEventListener('submit', function(event) {
-    event.preventDefault();
-    const userValue = event.target.todo.value;
-    todoArray.push({
-            text: userValue,
-            id: todoArray.length
-        });
-    event.target.reset();
-    renderList();
-});
-
-function createListItem(liObj) {
-    const li = document.createElement('li');
-    li.append(liObj.text);
-    li.dataset.id = liObj.id;
-    const button = document.createElement('button');
-    button.textContent = 'Kill me';
-    button.addEventListener('click', deleteHandler);
-    li.append(button);
-
-    return li;
+function tick() {
+    console.log('tack');
 }
 
 
-function renderList() {
-    const liArray = todoArray.map(el => createListItem(el));
-    list.replaceChildren(...liArray);
+
+tick();
+
+let j = 0;
+for (let i = 0; i < 100000000; i++) {
+    j++;    
 }
 
+console.log(j)
 
-function deleteHandler(event) {
-    const liId = Number(event.target.parentElement.dataset.id);
-    todoArray = todoArray.filter(liObj => liObj.id !== liId);
-    renderList();
-}
+clearTimeout(id);
