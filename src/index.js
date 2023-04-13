bf3c5ec38ed98cc40245c43a0f6e6fb2de3b30b7
freshcodeAlice <freshjs.ofte@gmail.com>
2023-04-13 10:32:58 +0300

    Promise first met

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
-setTimeout(function() {
-    console.log('timer tick!');
-    setTimeout(function () {
-        console.log('I need to be after timer');
-        setTimeout(function() {
-            console.log('another timer')
-        }, 1000);
-    }, 1000);
-}, 1000);
+const p = fetch('https://fakestoreapi.com/products');
+
+console.dir(p);
+
+p.then(function(responce) {
+    console.log('success!');
+    console.log(responce); // Responce - об'єкт відповіді, що є значенням проміса [[PromiseResult]]
+    /*
+    В об'єкті Responce маємо body, який лежить як набір байтів. Нам потрібно розпарсити цей body як
+    або текст
+    або formData (файл)
+    або json
 
 
-console.log('Sync work');
+    */
+   const p2 = responce.json();
+   p2.then(function(jsData) {
+        console.log(jsData);
+   }, function(error) {
+        console.log(error);
+   })
+
+}, function(error) {
+    console.log('error');
+    console.log(error);
+});
+
+
+//// Sync work
