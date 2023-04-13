6013fec63f6c891784402648ac66ac6f6949ff68
freshcodeAlice <freshjs.ofte@gmail.com>
2023-04-13 11:00:10 +0300

    then chaining

--- src/index.js
+++ src/index.js
@@ -2,28 +2,16 @@ const p = fetch('https://fakestoreapi.com/products');
 
 console.dir(p);
 
-p.then(function(responce) {
-    console.log('success!');
-    console.log(responce); // Responce - об'єкт відповіді, що є значенням проміса [[PromiseResult]]
-    /*
-    В об'єкті Responce маємо body, який лежить як набір байтів. Нам потрібно розпарсити цей body як
-    або текст
-    або formData (файл)
-    або json
-
-
-    */
-   const p2 = responce.json();
-   p2.then(function(jsData) {
-        console.log(jsData);
-   }, function(error) {
-        console.log(error);
-   })
-
-}, function(error) {
-    console.log('error');
+p
+.then(function(responce) {
+    return responce.json();
+    }, function(error) {
     console.log(error);
-});
-
+    })
+.then(function(jsData) {
+    console.log(jsData);
+    }, function(error) {
+    console.log(error);
+    })
 
 //// Sync work
