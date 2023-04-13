fb8410a637af86e9b0b80bd5abe6989d6c839293
freshcodeAlice <freshjs.ofte@gmail.com>
2023-04-13 11:22:34 +0300

    catch, finally

--- src/index.js
+++ src/index.js
@@ -4,14 +4,26 @@ console.dir(p);
 
 p
 .then(function(responce) {
+    console.log(responce);
     return responce.json();
-    }, function(error) {
-    console.log(error);
     })
 .then(function(jsData) {
     console.log(jsData);
-    }, function(error) {
-    console.log(error);
     })
+.catch(function(error) {
+    console.log(error);
+})
+.finally(function() {
+    console.log('FINALLY GET THERE')
+});
 
-//// Sync work
+/*
+Варіант 1:
+then має два коллбеки: перший виконується, якщо Promise -> resolve (fullfiled),
+                        другий виконується, якщо Promise -> rejected
+
+Варіант 2:
+then має 1 коллбек, який виконується, якщо Promise -> resolve (fullfiled)
+catch має 1 коллбек, який виконується, якщо в ланцюжку then-ів десь сталась помилка
+
+*/
