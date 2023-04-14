6e0a8fa1430610fc075b887b327500feede1ffab
freshcodeAlice <freshjs.ofte@gmail.com>
2023-04-14 10:47:34 +0300

    promisify loadImage

--- src/index.js
+++ src/index.js
@@ -1,24 +1,34 @@
+/* loadImage */
 
-/*
-Async/await
+//https://assets.entrepreneur.com/content/3x2/2000/20200429211042-GettyImages-1164615296.jpeg
 
 
-*/
+function loadImg(src) {
+    const img = document.createElement('img');
+    img.setAttribute('src', src);
 
-async function getServerData() {
-    /* Або try-catch */
-    const responce = await fetch('https://'); /*  В разі помилки await її просто викине */
-    const data = await responce.json();
-
-    return data;
+    return new Promise((resolve, reject) => {
+        img.addEventListener('load', (event) => {
+           resolve(img)
+        });
+        img.addEventListener('error', (event) => {
+            reject('Image cannot be loaded');
+        });
+    })
 }
 
-
-getServerData()
-.then((value) => {
-    console.log(value);
-}) 
-.catch(err => { /* Або обробка помилки проміса на поверненні результату */
-    console.log(err);
+const root = document.querySelector('#root');
+const div = document.createElement('div');
+div.classList.add('wrapper');
+root.append(div);
+const img = loadImg('https://.jpeg')
+.then((img) => {
+    div.append(img)
 })
+.catch((err) => {
+    console.error(err);
+});
 
+setTimeout(()=> {
+    console.log('code is working')
+}, 5000);
