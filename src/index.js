cfe46674d67381cc28ecb85ed972f186292022a1
freshcodeAlice <freshjs.ofte@gmail.com>
2023-04-14 09:50:07 +0300

    Promisify

--- src/index.js
+++ src/index.js
@@ -1,42 +1,16 @@
-/* Проміс зсередини */
-
-
-const p = new Promise(executor);
-
-function executor(resolve, reject) {
-   setTimeout(()=>{
-    resolve('its okay')
-   }, 2000);
-}
-console.log(p);
-
-
-p.then(function (value) {
-    console.log(value)
-}, function(error) {
-    console.log(error);
-})
-
-
-/*
-Написати проміс з котом Шредингера - в половині випадків проміс 
-має зарезолвитись, в половині - реджектнутись
-
-*/
-
-
-const catPromise = new Promise(function(resolve, reject){
+// Промісифікація функції 
+function loadFun() {
     if (Math.random() > 0.5) {
-        resolve('Cat is alive')
+        return new Promise((resolve)=>{
+            resolve(5);
+        });
     } else {
-        reject('Cat is dead');
+        return Promise.resolve(1);
     }
-})
+}
 
-console.log(catPromise);
 
-catPromise.then((value)=>{
-    console.log('RESOLVE ->', value)
-}, (error) => {
-    console.log('REJECT ->', error);
+const res = loadFun();
+res.then((value)=>{
+    console.log('PROMISE VALUE', value);
 })
